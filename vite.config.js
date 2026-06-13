e5160075a37fd7a7b6c6f957b457843899c89bb9
Cristian Habib <42555252+JungDefiant@users.noreply.github.com>
2026-06-12 23:05:10 -0700

    fix: removed explicit root

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/defiance-engine/',
-  root: '/defiance-engine/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
